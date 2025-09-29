ec6a85f3a0e0e2984c3f5bbc7ebdd6be910703ef
Austin Fraser <austinbfraser@gmail.com>
2025-09-29 15:22:15 -0700

    successfully calling openAI API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 // import Image from "next/image";
+import OpenAI from 'openai';
+
 import styles from './page.module.css';
 import { createClient } from '../../utils/supabase/server';
 import ReviewList from './components/ReviewList';
@@ -9,14 +11,33 @@ async function fetchReviews() {
   return res;
 }
 
+let cachedResponse: string | undefined;
+
 export default async function Home() {
   const res = await fetchReviews();
   const reviews = res.data;
 
+  if (!cachedResponse) {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const response = await openai.responses.create({
+      model: 'gpt-5-nano',
+      input: 'write a haiku about ai',
+      store: true,
+    });
+
+    const textOutput = response.output?.find((item) => 'content' in item);
+    cachedResponse = textOutput?.content?.[0]?.text ?? 'No haiku generated';
+    console.log('cachedResponse: ', cachedResponse)
+  }
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>AI Haiku:</h1>
+        <p>{cachedResponse}</p>
         {reviews && <ReviewList list={reviews}></ReviewList>}
       </main>
       <footer className={styles.footer}></footer>
